8f728c956b5134194f8ded7fcaad61d939d576c1
Alejandro Jiménez <a.j.agudo@gmail.com>
2016-03-07 21:08:23 +0100

    - Hotfix: minVersion and lastBuildVersion are now "optionals", but if they should existes (for example, if forceUpdate or otaUpdate is true), a Warning will be shown in debug console

--- AppliverySDK/Applivery/Model/Config.swift
+++ AppliverySDK/Applivery/Model/Config.swift
@@ -22,24 +22,42 @@ class Config {
 	var otaUpdateMessage: String?
 
 	
+	// MARK - Initializers
+	
 	convenience init(json: JSON) throws {
 		self.init()
 		
 		guard
 			let forceUpdate = json["sdk.ios.forceUpdate"]?.toBool(),
-			let minVersion	= json["sdk.ios.minVersion"]?.toString(),
 			let lastBuildId = json["sdk.ios.lastBuildId"]?.toString(),
-			let otaUpdate	= json["sdk.ios.ota"]?.toBool(),
-			let lastVersion	= json["sdk.ios.lastBuildVersion"]?.toString()
+			let otaUpdate	= json["sdk.ios.ota"]?.toBool()
 			else { throw ConfigError.ParseJson }
 		
+		
 		self.forceUpdate = forceUpdate
-		self.minVersion = minVersion
+		self.otaUpdate = otaUpdate
 		self.lastBuildId = lastBuildId
 		self.otaUpdate = otaUpdate
-		self.lastVersion = lastVersion
 		self.forceUpdateMessage = json["sdk.ios.mustUpdateMsg"]?.toString()
 		self.otaUpdateMessage	= json["sdk.ios.updateMsg"]?.toString()
+		self.minVersion = self.getParam("sdk.ios.minVersion", json: json, errorValue: "-1", shouldExists: self.forceUpdate)
+		self.lastVersion = self.getParam("sdk.ios.lastBuildVersion", json: json, errorValue: "-1", shouldExists: self.otaUpdate)
+	}
+	
+	
+	// MARK - Private Helpers
+	
+	/// get an "optional" param that should exists
+	private func getParam(parameter: String, json: JSON, errorValue: String, shouldExists: Bool) -> String {
+		guard let param = json[parameter]?.toString() else {
+			if shouldExists {
+				LogWarn("Error parsing JSON: \(parameter) parameter not found")
+			}
+			
+			return errorValue
+		}
+		
+		return param
 	}
 }
 
