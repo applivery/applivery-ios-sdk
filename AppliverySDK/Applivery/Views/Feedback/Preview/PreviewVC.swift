8836c0944e50ff277fa3d382043ec1c5a689c118
Alejandro Jiménez Agudo <a.j.agudo@gmail.com>
2016-11-27 23:12:17 +0100

    - Draw in preview

--- AppliverySDK/Applivery/Views/Feedback/Preview/PreviewVC.swift
+++ AppliverySDK/Applivery/Views/Feedback/Preview/PreviewVC.swift
@@ -8,20 +8,79 @@
 
 import UIKit
 
-class PreviewVC: UIViewController {
-
+class PreviewVC: UIViewController, UIGestureRecognizerDelegate {
+	
 	var screenshot: UIImage? {
 		didSet {
 			self.imageScreenshot?.image = self.screenshot
 		}
 	}
 	
+	var editedScreenshot: UIImage? {
+		get {
+			return self.imageScreenshot?.image
+		}
+	}
+	
+	// PRIVATE
+	private var brushWidth: CGFloat = 3
+	private var brushColor: UIColor = #colorLiteral(red: 0.8078431487, green: 0.2693349354, blue: 0.3300692421, alpha: 1)
+	private var lastPoint = CGPoint.zero
+	
+	// UI Properties
 	@IBOutlet private weak var imageScreenshot: UIImageView?
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
+	
+	// MARK: - View's life cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	
+	// MARK: - Gesture Delegate
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else {
+			return LogWarn("No touches found")
+		}
+		
+		self.lastPoint = touch.location(in: self.imageScreenshot)
+	}
+ 
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first  else {
+			return LogWarn("No touches found")
+		}
+		
+		let currentPoint = touch.location(in: view)
+		self.drawLineFrom(fromPoint: self.lastPoint, toPoint: currentPoint)
+		self.lastPoint = currentPoint
+	}
+	
+	func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+		guard let size = self.imageScreenshot?.frame.size else {
+			return LogWarn("Could not retrieved size")
+		}
+		
+		// Behan to draw
+		UIGraphicsBeginImageContext(size)
+		let context = UIGraphicsGetCurrentContext()
+		self.imageScreenshot?.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+		
+		// Draw the line in the context
+		context?.move(to: fromPoint)
+		context?.addLine(to: toPoint)
+		context?.setLineCap(.round)
+		context?.setLineWidth(self.brushWidth)
+		context?.setStrokeColor(self.brushColor.cgColor)
+		context?.setBlendMode(.normal)
+		context?.strokePath()
+		
+		// En the draw
+		self.imageScreenshot?.image = UIGraphicsGetImageFromCurrentImageContext()
+		self.imageScreenshot?.alpha = 1.0
+		UIGraphicsEndImageContext()
+	}
+	
 }
