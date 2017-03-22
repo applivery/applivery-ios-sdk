265e608359dbfc805157e8d414da773f538a23f3
Alejandro Jiménez Agudo <a.j.agudo@gmail.com>
2017-03-22 23:47:55 +0100

    - Customization: Added code documentaation for Palette class - Improved code documentation

--- AppliverySDK/Applivery/Applivery.swift
+++ AppliverySDK/Applivery/Applivery.swift
@@ -29,7 +29,7 @@ The Applivery's class provides the entry point to the Applivery service.
 
 ### Usage
 
-You should use the `sharedInstance` property to get a unique singleton instance, then set your `logLevel` configuration and finally call to the method:
+You should use the `shared` property to get a unique singleton instance, then set your `logLevel` configuration and finally call to the method:
 
 start(apiKey apiKey: String, appId: String, appStoreRelease: Bool)
 
@@ -43,13 +43,16 @@ When Applivery's starts, the latests configuration for your build will be retrie
 
 - seealso: [Applivery's README on GitHub](https://github.com/applivery/applivery-ios-sdk/blob/master/README.md)
 - Since: 1.0
-- Version: 2.2
+- Version: 2.4
 - Author: Alejandro Jiménez Agudo
 - Copyright: Applivery S.L.
 */
 public class Applivery: NSObject, StartInteractorOutput {
 	
-	/// Singleton instance. 
+	// MARK: - Type Properties
+	
+	/// Singleton instance.
+	///
 	/// - Warning: This property is **deprecated**. Use `shared` instead
 	@available(*, deprecated: 2.3, message: "Use shared instead", renamed: "shared")
 	public static let sharedInstance = Applivery()
@@ -57,13 +60,17 @@ public class Applivery: NSObject, StartInteractorOutput {
 	/// Singleton instance
 	public static let shared = Applivery()
 	
+	
+	// MARK: - Instance Properties
+	
 	/**
 	Type of Applivery's logs you want displayed in the debug console
 	
-	- **None**: No log will be shown. Recommended for production environments.
-	- **Error**: Only warnings and errors. Recommended for develop environments.
-	- **Info**: Errors and relevant information. Recommended for test integrating Applivery.
-	- **Debug**: Request and Responses to Applivery's server will be displayed. Not recommended to use, only for debugging Applivery.
+	* **none**: No log will be shown. Recommended for production environments.
+	* **error**: Only warnings and errors. Recommended for develop environments.
+	* **info**: Errors and relevant information. Recommended for test integrating Applivery.
+	* **debug**: Request and Responses to Applivery's server will be displayed. Not recommended to use, only for debugging Applivery.
+	
 	- Since: 1.0
 	- Version: 2.0
 	*/
@@ -76,7 +83,7 @@ public class Applivery: NSObject, StartInteractorOutput {
 	/**
 	Sets a color for the brush on screenshot edit mode
 	
-	- Warning: This property is **deprecated**. Use `palette.screenshotBrushColor` instead
+	- Warning: This property is **deprecated**. Use `Palette.screenshotBrushColor` instead
 	
 	- Since: 2.2
 	- Version: 2.4
@@ -90,6 +97,36 @@ public class Applivery: NSObject, StartInteractorOutput {
 	
 	/**
 	Customize the SDK colors to fit your app
+	
+	# Examples
+	
+	You can create a new instance of `Palette` and assign it to this property
+	
+	```swift
+	Applivery.shared.palette = Palette(
+		primaryColor: .orange,
+		secondaryColor: .white,
+		primaryFontColor: .white,
+		secondaryFontColor: .black,
+		screenshotBrushColor: .green
+	)
+	```
+	
+	The SDK has Applivery's colors by default so, if you only need to change the primary color, yo can do this:
+	
+	```swift
+	Applivery.shared.palette = Palette(
+		primaryColor: .orange,
+	)
+	```
+	
+	Or even directly change the property
+	
+	```swift
+	Applivery.shared.palette.primaryColor = .orange
+	```
+	
+	- seealso: `Palette`
 	- Since: 2.4
 	- Version: 2.4
 	*/
@@ -131,17 +168,17 @@ public class Applivery: NSObject, StartInteractorOutput {
 	}
 	
 	
-	// MARK: Public method
+	// MARK: Instance Methods
 	
 	/**
 	Starts Applivery's framework
 	
 	- Parameters:
-	- apiKey: Your developer's Api Key
-	- appId: Your application's ID
-	- appStoreRelease: Flag to mark the build as a build that will be submitted to the AppStore. This is needed to prevent unwanted behavior like prompt to a final user that a new version is available on Applivery.
-	* True: Applivery will stop any activity. **Use this for AppStore**
-	* False: Applivery will works as normally. Use this with distributed builds in Applivery.
+		- apiKey: Your developer's Api Key
+		- appId: Your application's ID
+		- appStoreRelease: Flag to mark the build as a build that will be submitted to the AppStore. This is needed to prevent unwanted behavior like prompt to a final user that a new version is available on Applivery.
+			* `true`: Applivery will stop any activity. **Use this for AppStore**
+			* `false`: Applivery will works as normally. Use this with distributed builds in Applivery.
 	
 	- Attention: Be sure that the param **appStoreRelease** is true before submitting to the AppStore
 	- Since: 1.0
--- AppliverySDK/Applivery/Model/Palette.swift
+++ AppliverySDK/Applivery/Model/Palette.swift
@@ -8,25 +8,74 @@
 
 import Foundation
 
+
+/**
+A color palette for view's customization
+
+# Overview
+You can customize the SDK's colors by setting the properties of this class.
+
+# Example
+
+```swift
+Applivery.shared.palette = Palette(
+	primaryColor: .orange,
+	secondaryColor: .white,
+	primaryFontColor: .white,
+	secondaryFontColor: .black,
+	screenshotBrushColor: .green
+)
+```
+
+and then set the instance to `Applivery.shared.palette`
+
+- seealso: `Applivery.palette`
+- Since: 2.4
+- Version: 2.4
+- Author: Alejandro Jiménez Agudo
+- Copyright: Applivery S.L.
+*/
 public class Palette: NSObject {
+	
+	// MARK: - Instance Properties
+	
+	/// Main color of your brand
 	public var primaryColor: UIColor
+	
+	/// Background color
 	public var secondaryColor: UIColor
+	
+	/// Primary font color. It should be in contrast with the primary color
 	public var primaryFontColor: UIColor
+	
+	/// Secondary font color. It should be in contrast with the secondary color
 	public var secondaryFontColor: UIColor
+	
+	/// Sets a color for the brush on screenshot edit mode
 	public var screenshotBrushColor: UIColor
 	
-	// MARK: - Applivery's colors
-	private static let appliveryPrimary			= #colorLiteral(red: 0.1137254902, green: 0.4392156863, blue: 0.7176470588, alpha: 1)
-	private static let appliveryPrimaryFont		= #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-	private static let appliverySecondary		= #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-	private static let appliverySecondaryFont	= #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1)
-	private static let appliveryScreenshotBrush = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
 	
-	public init(primaryColor: UIColor = Palette.appliveryPrimary,
-	            secondaryColor: UIColor = Palette.appliveryPrimaryFont,
-	            primaryFontColor: UIColor = Palette.appliverySecondary,
-	            secondaryFontColor: UIColor = Palette.appliverySecondaryFont,
-	            screenshotBrushColor: UIColor = Palette.appliveryScreenshotBrush) {
+	// MARK: - Initializer
+	
+	/**
+	Creates a new instance of an Applivery's palette.
+	
+	- Parameters:
+		- primaryColor: Main color of your brand
+		- secondaryColor: Background color
+		- primaryFontColor: Primary font color. It should be in contrast with the primary color
+		- secondaryFontColor: Secondary font color. It should be in contrast with the secondary color
+		- screenshotBrushColor: Sets a color for the brush on screenshot edit mode
+	
+	- Note: Each parameter has a default color, so you could set only the values that you really need to change
+	- Since: 2.4
+	- Version: 2.4
+	*/
+	public init(primaryColor: UIColor = Colors.appliveryPrimary,
+	            secondaryColor: UIColor = Colors.appliveryPrimaryFont,
+	            primaryFontColor: UIColor = Colors.appliverySecondary,
+	            secondaryFontColor: UIColor = Colors.appliverySecondaryFont,
+	            screenshotBrushColor: UIColor = Colors.appliveryScreenshotBrush) {
 		self.primaryColor = primaryColor
 		self.primaryFontColor = primaryFontColor
 		self.secondaryColor = secondaryColor
