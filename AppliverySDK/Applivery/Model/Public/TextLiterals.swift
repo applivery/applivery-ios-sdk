8e51559a01d5850894514d8fe2536c6a521e04b2
Alejandro Jiménez Agudo <a.j.agudo@gmail.com>
2017-03-26 20:21:43 +0200

    - Custom Literals: Feedback view’s literals can be passed by params

--- AppliverySDK/Applivery/Model/Public/TextLiterals.swift
+++ AppliverySDK/Applivery/Model/Public/TextLiterals.swift
@@ -20,6 +20,16 @@ public class TextLiterals: NSObject {
 	// MARK: - Force Update
 	public var forceUpdateMessage: String?
 	public var buttonForceUpdate: String = localize("update_view_button_update")
+	
+	// MARK: - Feedback
+	public var feedbackButtonClose: String = localize("feedback_button_close")
+	public var feedbackButtonAdd: String = localize("feedback_button_add")
+	public var feedbackButtonSend: String = localize("feedback_button_send")
+	public var feedbackSelectType: String = localize("feedback_label_select_type")
+	public var feedbackTypeBug: String = localize("feedback_button_bug")
+	public var feedbackTypeFeedback: String = localize("feedback_button_feedback")
+	public var feedbackMessagePlaceholder: String = localize("feedback_text_message_placeholder")
+	public var feedbackAttach: String = localize("feedback_label_attach")
 }
 
 enum Literal: CustomStringConvertible {
@@ -29,12 +39,21 @@ enum Literal: CustomStringConvertible {
 	case alertButtonUpdate
 	case forceUpdateMessage
 	case buttonForceUpdate
+	case feedbackButtonClose
+	case feedbackButtonAdd
+	case feedbackButtonSend
+	case feedbackSelectType
+	case feedbackTypeBug
+	case feedbackTypeFeedback
+	case feedbackMessagePlaceholder
+	case feedbackAttach
 	
 	var description: String {
 		return literal(self) ?? String(self.hashValue)
 	}
 }
 
+//swiftlint:disable cyclomatic_complexity
 func literal(_ literal: Literal) -> String? {
 	let literals = GlobalConfig.shared.textLiterals
 	
@@ -45,5 +64,14 @@ func literal(_ literal: Literal) -> String? {
 	case .alertButtonUpdate: return literals.alertButtonUpdate
 	case .forceUpdateMessage: return literals.forceUpdateMessage
 	case .buttonForceUpdate: return literals.buttonForceUpdate
+	case .feedbackButtonClose: return literals.feedbackButtonClose
+	case .feedbackButtonAdd: return literals.feedbackButtonAdd
+	case .feedbackButtonSend: return literals.feedbackButtonSend
+	case .feedbackSelectType: return literals.feedbackSelectType
+	case .feedbackTypeBug: return literals.feedbackTypeBug
+	case .feedbackTypeFeedback: return literals.feedbackTypeFeedback
+	case .feedbackMessagePlaceholder: return literals.feedbackMessagePlaceholder
+	case .feedbackAttach: return literals.feedbackAttach
 	}
 }
+//swiftlint:enable cyclomatic_complexity
