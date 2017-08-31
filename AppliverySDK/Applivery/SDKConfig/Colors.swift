bfa0e55f255e77aec8ccd520645e750874dcfb1d
Alejandro Jiménez Agudo <a.j.agudo@gmail.com>
2017-08-31 23:43:02 +0200

    - Fix: Made public Color and Localize because the were used as default value y public initializers

--- AppliverySDK/Applivery/SDKConfig/Colors.swift
+++ AppliverySDK/Applivery/SDKConfig/Colors.swift
@@ -8,11 +8,10 @@
 
 import Foundation
 
-
-struct Colors {
-	static let appliveryPrimary			= #colorLiteral(red: 0.1137254902, green: 0.4392156863, blue: 0.7176470588, alpha: 1)
-	static let appliveryPrimaryFont		= #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-	static let appliverySecondary		= #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-	static let appliverySecondaryFont	= #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1)
-	static let appliveryScreenshotBrush = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+public struct Colors {
+	public static let appliveryPrimary			= #colorLiteral(red: 0.1137254902, green: 0.4392156863, blue: 0.7176470588, alpha: 1)
+	public static let appliveryPrimaryFont		= #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+	public static let appliverySecondary		= #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+	public static let appliverySecondaryFont	= #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1)
+	public static let appliveryScreenshotBrush 	= #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
 }
