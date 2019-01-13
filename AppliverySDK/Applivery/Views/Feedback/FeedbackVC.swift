7288865c301e4c1c195bd79fdccc4b159ade108a
Alejandro Jiménez Agudo <a.j.agudo@gmail.com>
2019-01-13 21:17:23 +0100

    Fix: Landscape screenshots doesn't work properly. Force feedback formulary to portrait orientation

--- AppliverySDK/Applivery/Views/Feedback/FeedbackVC.swift
+++ AppliverySDK/Applivery/Views/Feedback/FeedbackVC.swift
@@ -67,6 +67,10 @@ extension FeedbackVC {
 		return .lightContent
 	}
 	
+	override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		return UIInterfaceOrientationMask.portrait
+	}
+	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		guard let previewVC = segue.destination as? PreviewVC else {
 			return logWarn("Expected PreviewVC")
@@ -158,7 +162,6 @@ extension FeedbackVC: FeedbackView {
 	
 	private func startScreenshotAnimated() {
 		let screenshotCopy = UIImageView(image: self.previewVC?.editedScreenshot)
-		screenshotCopy.frame = self.view.frame
 		screenshotCopy.contentMode = .scaleAspectFit
 		self.view.addSubview(screenshotCopy)
 		self.feedbackForm.alpha = 0
@@ -332,6 +335,15 @@ extension FeedbackVC {
 		self.manageKeyboardShowEvent()
 		self.manageKeyboardHideEvent()
 		
+		let width = UIScreen.main.bounds.width
+		let height = UIScreen.main.bounds.height
+		var ratio: CGFloat
+		if width > height {
+			ratio = height / width
+		} else {
+			ratio = width / height
+		}
+		
 		self.screenshotContainer.translatesAutoresizingMaskIntoConstraints = false
 		self.screenshotContainer.addConstraint(NSLayoutConstraint(
 			item: self.screenshotContainer,
@@ -339,7 +351,7 @@ extension FeedbackVC {
 			relatedBy: NSLayoutRelation.equal,
 			toItem: self.screenshotContainer,
 			attribute: NSLayoutAttribute.height,
-			multiplier: UIScreen.main.bounds.width / UIScreen.main.bounds.height,
+			multiplier: ratio,
 			constant: 0
 		))
 	}
