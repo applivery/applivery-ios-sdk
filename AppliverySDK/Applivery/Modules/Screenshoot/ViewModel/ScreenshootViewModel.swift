49ecde5ce93a2ae00f1f7372e2e1b0ac2a324813
alexkater <alejandro@controlsoftware.co>
2024-09-13 12:13:48 -0500

    Working Wip

--- AppliverySDK/Applivery/Modules/Screenshoot/ViewModel/ScreenshootViewModel.swift
+++ AppliverySDK/Applivery/Modules/Screenshoot/ViewModel/ScreenshootViewModel.swift
@@ -34,31 +34,6 @@ final class ScreenshootViewModel {
         }
     }
     
-    func saveDrawing(image: UIImage, lines: [Line]) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(image.size, false, image.scale)
-        guard let context = UIGraphicsGetCurrentContext() else {
-            UIGraphicsEndImageContext()
-            return nil
-        }
-        
-        image.draw(in: CGRect(origin: .zero, size: image.size))
-        
-        for line in lines {
-            context.setStrokeColor(line.color.cgColor ?? CGColor(red: 1, green: 1, blue: 1, alpha: 1))
-            context.setLineWidth(line.lineWidth)
-            context.setLineCap(.round)
-            
-            let cgPath = line.path.cgPath
-            
-            context.addPath(cgPath)
-            context.drawPath(using: .fillStroke)
-        }
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
     func exportDrawing(image: UIImage, lines: [Line]) -> UIImage {
         
         // Create a context of the starting image size and set it as the current one
