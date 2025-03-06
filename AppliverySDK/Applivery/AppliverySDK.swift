6083cdbcb8f0f955a79ae68d2f46c5505852dfb8
Fran Alarza <franalarza@gmail.com>
2025-03-06 18:28:22 +0100

    set version

--- AppliverySDK/Applivery/AppliverySDK.swift
+++ AppliverySDK/Applivery/AppliverySDK.swift
@@ -121,7 +121,7 @@ public class AppliverySDK: NSObject {
     
     // MARK: - Static Properties
     
-    internal static let sdkVersion = "4.3.8"
+    internal static let sdkVersion = "4.3.7"
     
     // MARK: - Type Properties
     
