09c61daf17c2c5bdaa0aa795170f6fe31a46652b
Alejandro Arjonilla <alejandro@applivery.com>
2025-04-15 09:16:29 +0200

    feat: Provide possibility to "force authentication" (disabling "Cancel") https://github.com/applivery/applivery-ios-sdk/issues/72

--- AppliverySDK/Model/ConfigurationUpdate.swift
+++ AppliverySDK/Model/ConfigurationUpdate.swift
@@ -7,14 +7,30 @@
 
 import Foundation
 
+@objcMembers
 public class AppliveryConfiguration: NSObject {
-    let postponedTimeFrames: [TimeInterval]
-    
-    public init(postponedTimeFrames: [TimeInterval] = []) {
+    public let postponedTimeFrames: [TimeInterval]
+    public let enforceAuthentication: Bool
+
+    public init(
+        postponedTimeFrames: [TimeInterval] = [],
+        enforceAuthentication: Bool = false
+    ) {
         self.postponedTimeFrames = postponedTimeFrames
+        self.enforceAuthentication = enforceAuthentication
+    }
+
+    @objc(initWithPostponedTimeFramesNSNumber:enforceAuthentication:)
+    public convenience init(
+        postponedTimeFrames nsNumbers: [NSNumber],
+        enforceAuthentication: Bool
+    ) {
+        self.init(
+            postponedTimeFrames: nsNumbers.map { $0.doubleValue },
+            enforceAuthentication: enforceAuthentication
+        )
     }
 }
-
 extension AppliveryConfiguration {
     public static let empty = AppliveryConfiguration()
 }
