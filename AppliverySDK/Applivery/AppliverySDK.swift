144911c4690f9c39dda28e93c6f67fc8191a002f
Abby Dominguez <abbytorade@icloud.com>
2025-11-14 13:25:41 +0100

    chore: update version in comment

--- AppliverySDK/Applivery/AppliverySDK.swift
+++ AppliverySDK/Applivery/AppliverySDK.swift
@@ -118,8 +118,7 @@ public typealias AppliveryLogHandler = @convention(block) (
 
  - SeeAlso: [Applivery's README on GitHub](https://github.com/applivery/applivery-ios-sdk/blob/master/README.md)
  - Since: 1.0
- - Version: 4.5.0
- - Author: Alejandro Jiménez Agudo
+ - Version: 4.5.3
  - Copyright: Applivery S.L.
  */
 public class AppliverySDK: NSObject, AppliveryService {
